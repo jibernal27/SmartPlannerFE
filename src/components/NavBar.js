c11c598a4ab750cb304eed1a114cc8f879d89faf
dalthviz <d.althviz10@uniandes.edu.co>
2017-03-05 00:49:42 -0500

    Arreglos en la barra de navegación.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,22 +9,22 @@ class NavBar extends Component {
 
 	render() {
 		return(
-<nav className="navbar navbar-inverse">
+<nav className="navbar navbar-default">
   <div className="container-fluid">
     <div className="navbar-header">
-      <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+				<span className="sr-only">Toggle navigation</span>
+				<span className="icon-bar"></span>
         <span className="icon-bar"></span>
         <span className="icon-bar"></span>
-        <span className="icon-bar"></span> 
       </button>
       <a className="navbar-brand" href="#">
       			<img alt="Smart Planner" src="./style/imgs/smartPlanner.png" className="logo_img" />
       </a>
     </div>
-    <div className="collapse navbar-collapse" id="myNavbar">
+    <div className="navbar-collapse collapse" id="myNavbar">
       <ul className="nav navbar-nav navbar-right">
-      	<li>prueba</li>
-        <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      	<li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
         <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
       </ul>
     </div>
@@ -45,7 +45,7 @@ export default NavBar;
       <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span className="icon-bar"></span>
         <span className="icon-bar"></span>
-        <span className="icon-bar"></span> 
+        <span className="icon-bar"></span>
       </button>
       <a className="navbar-brand" href="#">
       			<img alt="Smart Planner" src="./style/imgs/smartPlanner.png" className="logo_img" />
