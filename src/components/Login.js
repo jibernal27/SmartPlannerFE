71523787d956dc04e6d59735b3cc0d783f60d2c3
jdfandino10 <jd.fandino10@uniandes.edu.co>
2017-03-05 01:00:30 -0500

    login view done

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,15 +18,22 @@ class Login extends Component {
 
 	render() {
             return(
-	  <div className={this.state.show +' login'}>
-	  <div className='loginOptions'>
-	  	<h2>Seleccione usuario</h2>
+      <div className={this.state.show+' login'}>
+      <div className='row'>
+      	<img src='./style/imgs/smartPlanner.png' className='login-logo'/>
+      </div>
+	  <div className='row'>
+	  <div className='col-xs-1 col-md-4'></div>
+	  <div className='col-xs-10 col-md-4'>
+	  	<h2>Seleccione un usuario</h2>
 	  	<button className='btn btn-default' onClick={() => this.getUser('Jose', (obj)=>{
 	  		this.setUser(obj);
 	  	})}>JDF</button>
 	  	<button className='btn btn-default' onClick={() => this.getUser('Daniel', (obj)=>{
 	  		this.setUser(obj);
 	  	})}>DA</button>
+	  </div>
+	  <div className='col-xs-1 col-md-4'></div>
 	  </div>
 	  </div>
 );
